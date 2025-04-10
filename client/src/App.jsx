b63f85e261179e4e1e4ad4590f83187aac5ec262
aniket-kangane <aniketkangane@pepsales.co>
2025-04-10 23:04:14 +0530

    shadcn setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <Routes>
+      {/* LANDING PAGE */}
+      <Route path="/" element={<Landing />} />
+
+      {/* USERS PAGES */}
+
+      {/* REPRESENTATIVE PAGES */}
+
+      {/* ADMIN PAGES */}
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,15 @@
+import { ArrowRight } from "lucide-react";
+import { Button } from "../components/ui/button";
+
+function Landing() {
+  return (
+    <div>
+      <p>Welcome to landin pages</p>
+      <Button>
+        Click Me <ArrowRight />
+      </Button>
+    </div>
+  );
+}
+
+export default Landing;
